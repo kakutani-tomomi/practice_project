5c19b511453afd2657c3b19926dd6cbf761c091a
DYNABOOK-B552\TOSHIBA <tomomi.kakutani01@gmail.com>
2023-12-22 11:03:29 +0900

    メソッド練習

---
+++ src/main/java/method/q01/Greeting.java
@@ -0,0 +1,13 @@
+package method.q01;
+
+public class Greeting {
+
+	public static void main(String[] args) {
+		sayhello();
+	}
+
+	public static void sayhello() {
+		System.out.println("Hello,Java!!");
+	}
+
+}
---
+++ src/main/java/method/q02/Introduse.java
@@ -0,0 +1,14 @@
+package method.q02;
+
+public class Introduse {
+
+	public static void main(String[] args) {
+		String name = "山田";
+		selfintroduse(name);
+
+	}
+
+	public static void selfintroduse(String name) {
+		System.out.println("私の名前は" + name + "です。");
+	}
+}
